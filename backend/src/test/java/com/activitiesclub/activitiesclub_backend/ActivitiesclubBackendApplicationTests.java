fdfb6d1da3968355410d3053b9554730ed575fb6
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2026-02-16 10:39:44 +0000

    initialize springboot backend

---
+++ backend/src/test/java/com/activitiesclub/activitiesclub_backend/ActivitiesclubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitiesclub.activitiesclub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiesclubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
